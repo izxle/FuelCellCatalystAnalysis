696df528929a11afee11590e89ff67fdc23f6080
izxle <oscarxavier.ox@gmail.com>
2014-11-28 14:09:33 -0600

    Core files

---
+++ catCaract.py
@@ -0,0 +1,53 @@
+from getdata2 import *
+from electrode import *
+from re import search
+from report import Report
+from arraytoexcel import toClipboardForExcel
+import CV, CO, ORR
+import matplotlib.pyplot as plt
+import sys
+#init
+def runAll(fpath=None, ext = ".txt", COnam=None, CVnam=None, bCV=True, bCO=True,
+            mCat=5., vSolv=2., pCatCen=30., area=0.196, vInk=10., addH=True,
+            CVsr=50., CVrangLow=0.4, CVrangHigh=0.6, COrangCOHigh=1.,
+            COsr=20., COrangCLow=0.4, COrangCHigh=0.6, COrangCOLow=0.6,
+            ORRrangLow=0.2, ORRrangHigh=0.4, ORRshift=1.0, ORRsr=20., bORR=True,
+            verb=False, graph=[0,0,0,0], copy=False, MEA=False):
+    #graph = [CV, CO, Tafel, KL]
+    #init
+    #electrode and ink data
+    ink = Ink(mCat=mCat, vSolv=vSolv, pCatCen=pCatCen)
+    WE = Electrode(area=0.196, ink=ink, vInk=vInk)
+    #get filenames cn folder
+    folder = Folder(fpath=fpath, ext=ext)
+    #TODO: get useful file names
+    nams = folder.getNams()
+    if verb: print nams
+    #TODO: mejorar
+    #runs
+    if bCV:
+        Vars = {'sr': CVsr, 'rCl': CVrangLow, 'rCu': CVrangHigh,
+                'copy': copy, 'graph': graph[0]}
+        CVpath = '\\' + CVnam + ext
+        cCV = folder.getCycles(CVpath, last=True)
+        aCV = CV.H(cCV, **Vars)
+        WE.area.update(CV=aCV)
+    if bCO:
+        Vars = {'sr': COsr, 'rCl':COrangCLow, 'rCu': COrangCHigh, 'graph': graph[1],  
+                'rCOl': COrangCOLow, 'rCOu': COrangCOHigh, 'copy': copy, 'addH': addH}
+        COpath = '\\' + COnam + ext
+        cCO = folder.getCycles(COpath)
+        aCO, aH = CO.run(cCO, **Vars)
+        WE.area.update(CO=aCO, H=aH)
+    if bCV or bCO: WE.setECSA()
+    if bORR:
+        Vars = {'sr': ORRsr, 'rL': ORRrangLow, 'rU': ORRrangHigh,
+                'par': ORRshift, 'copy': copy, 'graph': graph[2]}
+        ORRfiles = {int(search("[0-9]+00", nam).group(0)): nam for nam in nams if "00" in nam}
+        cTafel = folder.getCycles(ORRfiles[1600])[1]
+        #TODO: get ciclo util
+        WE.setActs(ORR.tafel(cTafel, WE, **Vars))
+        #KL
+        cORR = {val: folder.getCycles(nam)[1] for val, nam in ORRfiles.items()}
+        WE.setKL(ORR.KL(cORR, WE, graph=graph[3], copy=copy))
+    return WE
---
+++ getdata2.py
@@ -0,0 +1,136 @@
+import glob
+from re import split as resplit
+import matplotlib.pyplot as plt
+import numpy as np
+    
+    
+reSep = "\s+"
+pat = "\x00" #some txt hav it @ end
+
+#TODO: maybe add a File class: getdata, getcycles, graph
+
+#for files from AutoLab
+class Folder(object):
+    def __init__(self, fpath='', files=[], ext='.txt', verb=True):
+        """
+        get data from files from path of folder
+        """
+        self.verb = verb
+        self.ext = ext
+        self.fpath = fpath
+        self.files = {nam.lstrip(self.fpath): {'cycles': None} for nam in (
+                            self._getFiles() if not files else files)}
+        self._getData()
+        self._getCycles()
+    
+    def _getFiles(self):
+        #TODO: exclude folders
+        return glob.glob(self.fpath + '/*' + self.ext)
+    
+    def getFiles(self):
+        return [self.fpath + nam for nam in self.files]
+        
+    def getNams(self):
+        return self.files.keys()
+        
+    def _readFile(self, path):
+        #get data from file to float array skipping first row
+        #deleted keys, not used
+        kw = {'skiprows':1}
+        if '.xls' in self.ext:
+            kw['delimiter'] = ';'
+        try:
+            if self.verb: print "Loading", path.lstrip(self.fpath),
+            data = np.loadtxt(path, **kw)
+            if self.verb: print "... done."
+        except Exception, e:
+            if self.verb: print
+            print e, '1234123412342341'
+        #TODO: maybe chk 4 mas str
+        return data
+
+    def _getData(self):
+        for path in self.getFiles():
+            nam = path.lstrip(self.fpath)
+            try:
+                #mark data?
+                self.files[nam]['data'] = self._readFile(path)
+            except:
+                if self.verb: print ".. deleting."
+                del self.files[nam]
+    
+    def _getCycles(self):
+        if self.verb: print "file   cycles"
+        for nam in self.files:
+            data = self.files[nam]["data"]
+            #TODO: mejorar
+            cycles = {}            
+            if '.xls' in self.ext:
+                for potential, time, current, scan, index, wepotential in data:
+                    cycles.update({int(scan): cycles.get(int(scan), []) + [(wepotential, current)]})
+            else:
+                scan = 1
+                #TODO: chk fwd
+                #TODO: mejorar, xtra cycle
+                fwd = True #needed?
+                init = data[0][0] #first potential
+                last = init
+                ####
+                for potential, current, time in data:
+                    #toggle fwd flag
+                    if not fwd:
+                        if (potential - last) > 0: fwd = True
+                        #if bkwrd y just past init, scan++
+                        elif potential<=init and last>init: scan += 1
+                    elif (potential - last) < 0:
+                        fwd = False
+                    #TODO: better way?
+                    #cycles.update({int(scan): cycles.get(int(scan), []) + [(potential, current)]})
+                    #append potencial, corriente
+                    try: cycles[scan].append((potential, current))
+                    #if no key, crear
+                    except KeyError: cycles[scan] = [(potential,current)]
+                    last = potential
+            for k in cycles: cycles[k] = np.array(cycles[k])
+            self.files[nam]["cycles"] = cycles
+            #fix vvv
+            if self.verb: print nam[-17:-10], scan if '.xls' in self.ext else scan-1
+    
+    def getCycles(self, nam, last=False):
+        if last:
+            cycle = len(self.files[nam]["cycles"].keys())-1
+            return self.files[nam]["cycles"][cycle]
+        else:
+            return self.files[nam]["cycles"]
+#        try:
+#            #TODO: mejorar
+#            if last:
+#                cycle = len(self.files[nam]["cycles"].keys())-1
+#                return self.files[nam]["cycles"][cycle]
+#            else:
+#                return self.files[nam]["cycles"]
+#        except Exception, e:
+#            print e
+#            print nam
+#            print self.files[nam]
+#            print 'error', self.files.keys()
+#
+#
+#..
+
+
+
+#def graph(path):
+#    keys, data = readFile(path)
+#    plt.figure()
+#    plt.plot(*zip(*data))
+#    plt.show()
+
+#module stuff ?????
+if __name__=="__main__":
+    pass
+else:
+    pass
+    #del resplit
+    #del glob
+    #del reSep
